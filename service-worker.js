a83fc38163f087516b432ff12892d3d938f9f241
William Hardy <whardy@vagabond-group.com>
2026-09-21 14:04:30 -0700

    Add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="bw-shows-v1";
+const CORE=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
