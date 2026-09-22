a6bdf523134fb9ec3111c77212a4050332e262ec
William Hardy <whardy@vagabond-group.com>
2026-09-21 19:46:03 -0700

    Refresh cache for automatic status tracking

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="bw-shows-v8";
+const CACHE="bw-shows-v9";
 const CORE=["./manifest.webmanifest","./config.js"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
